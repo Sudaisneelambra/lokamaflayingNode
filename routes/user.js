2e36816743c68dc57b08c1e492e0935e51b5aa14
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-01-30 18:03:15 +0530

    second commit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,22 @@
+// requiring express
 const express =require('express')
 
+// using express router
 const router=express.Router()
+
+// requiring mongoose
+const mongoose=require('mongoose')
+
+// requiring dotenv
+require('dotenv').config()
+
+// requiring mongodb url
+
+const DB_URL=process.env.DB_connection
+
+// connecting mongodb
+
+mongoose.connect(DB_URL)
 
 router.get('/signup',()=>{
 console.log("sudais neelambra");
